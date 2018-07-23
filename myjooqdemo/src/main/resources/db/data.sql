9309e80a4e099a639cf4676ae432a1a00946dbd2
zhangjmhappy <244820075@qq.com>
2018-07-24 01:52:38 +0800

    测试数据

---
+++ myjooqdemo/src/main/resources/db/data.sql
@@ -0,0 +1,10 @@
+USE `test_demo`;
+
+/*Data for the table `shop` */
+
+insert  into `shop`(`id`,`shop_name`,`contact_name`,`contact_phone`) values 
+
+(1,'美宜佳便利店','张先生','13500001111'),
+
+(2,'好吃再来麻辣烫','王先生','13522221212');
+
